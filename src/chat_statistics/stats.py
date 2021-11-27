b378d195eabc094548419749fd7077971c7db9c6
sina karaji <karaji.sina@gmail.com>
2021-11-27 13:59:33 +0330

    Extract all text from JSON file without emojis

--- src/chat_statistics/stats.py
+++ src/chat_statistics/stats.py
@@ -1,7 +1,10 @@
 import json
+import re
 from pathlib import Path
 from typing import Union
+from collections import Counter
 
+import demoji
 import arabic_reshaper
 from bidi.algorithm import get_display
 from hazm import Normalizer, word_tokenize
@@ -29,7 +32,28 @@ class ChatStatistics:
         stop_words = open(DATA_DIR / 'stopwords.txt').readlines()
         stop_words = list(map(str.strip, stop_words))
         self.stop_words = list(map(self.normalizer.normalize, stop_words))
+        
+    def de_emojify(self, text):
+        """Removes emojis and some special characters from the text. 
 
+        :param text: Text that contains emoji 
+        """
+        regrex_pattern = re.compile(pattern = "["
+            "\u2069"
+            "\u2066"
+                    "]+", flags = re.UNICODE)
+        text = regrex_pattern.sub(r'', text)
+        return demoji.replace(text, " ")
+    
+    def remove_stopwords(self, text):
+        """Removes stop-words from the text. 
+
+        :param text: Text that may contain stop-words. 
+        """
+        tokens = word_tokenize(self.normalizer.normalize(text))
+        tokens = list(filter(lambda item: item not in self.stop_words, tokens))
+        return ' '.join(tokens)
+        
     def generate_word_cloud(
         self,
         output_dir: Union[str, Path],
@@ -43,25 +67,34 @@ class ChatStatistics:
         """
         logger.info("Loading text content...")
         text_content = ''
-        for msg in self.chat_data['messages']:
-            if type(msg['text']) is str:
-                tokens = word_tokenize(msg['text'])
-                tokens = list(filter(lambda item: item not in self.stop_words, tokens))
-                text_content += f" {' '.join(tokens)}"
+        messages = iter(self.chat_data['messages'])
+        for message in messages:
+            msg = message['text']
+            if isinstance(msg, list):
+                for sub_msg in msg:
+                    if isinstance(sub_msg, str):
+                        text_content += f" {self.remove_stopwords(sub_msg)}"
+                    elif isinstance(sub_msg, dict) and sub_msg['type'] in {
+                        'text_link', 'bold', 'italic', 'hashtag', 'mention', 'pre'}:
+                        text_content += f" {self.remove_stopwords(sub_msg['text'])}"
+            else:    
+                text_content += f" {self.remove_stopwords(msg)}"
 
         # normalzie, reshape for final word cloud
-        text_content = self.normalizer.normalize(text_content)
-        text_content = arabic_reshaper.reshape(text_content)
+        text_content = arabic_reshaper.reshape(self.de_emojify(text_content))
         text_content = get_display(text_content)
+        # tokenizing the final text content
+        list_data = Counter(word_tokenize(text_content)).most_common()
+        dict_data = {x[0]: x[1] for x in list_data}
 
         logger.info("Generating word cloud...")
         # generate word cloud
         wordcloud = WordCloud(
             width=1200, height=1200,
-            font_path=str(DATA_DIR / 'BHoma.ttf'),
+            font_path=str(DATA_DIR / 'Vazir.ttf'),
             background_color=background_color,
             max_font_size=250
-        ).generate(text_content)
+        ).generate_from_frequencies(dict_data)
 
         logger.info(f"Saving word cloud to {output_dir}")
         wordcloud.to_file(str(Path(output_dir) / 'wordcloud.png'))
