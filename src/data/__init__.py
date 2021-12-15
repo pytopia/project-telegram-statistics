c465cdc3a0f2411f403cb25239370c573efdf71a
sina karaji <karaji.sina@gmail.com>
2021-12-15 22:30:46 +0330

    DATA_DIR were returned to absolute path.

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,5 +1,3 @@
 from pathlib import Path
 
-
-DATA_DIR_ABSOLUTE = Path(__file__).resolve().parent
-DATA_DIR = DATA_DIR_ABSOLUTE.relative_to(DATA_DIR_ABSOLUTE.parents[1])
+DATA_DIR = Path(__file__).resolve().parent
